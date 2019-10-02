be4c5f86ec84bbfa0a1cb3a35a31f0bddd54d3a9
Wan Firdaus <wanfirdaus91@gmail.com>
2019-10-02 12:22:06 +0800

    add gitignore

--- express/app.js
+++ express/app.js
@@ -1,5 +1,9 @@
+const Joi = require('joi');
 const express = require('express');
+
 const app = express();
+
+app.use(express.json());
 
 //main method in express
 // app.get();
@@ -7,21 +11,64 @@ const app = express();
 // app.put();
 // app.delete();
 
+const courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'},
+]
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found');
+    res.send(course);
 });
 
 app.get('/api/posts/:year/:month', (req, res) => {
     res.send(req.query);
 });
+
+app.post('/api/courses', (req, res) => {
+
+    const schema = {
+        name : Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+
+    if (result.error) {
+        //400 bad request
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name, //to use this must enable express.json()
+    };
+
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    // look up the course
+    // if not exist, return 404
+
+    //Validate
+    //if invalid, return 404 - Bad Request
+
+    //Update course
+    //Return the updated course
+})
+
+
+
 
 //PORT
 const port = process.env.PORT || 3000;
